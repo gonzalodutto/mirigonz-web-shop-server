a0d9132382ab9d2f0b4d59916cc8799e8baa88ae
gonzalodutto <gonzalodutto@gmail.com>
2022-07-20 16:37:53 +0200

    Seed users updated

--- seeders/20220718121605-some-users.js
+++ seeders/20220718121605-some-users.js
@@ -1,4 +1,5 @@
 "use strict";
+const bcrypt = require("bcrypt");
 
 module.exports = {
   async up(queryInterface, Sequelize) {
@@ -8,7 +9,24 @@ module.exports = {
         {
           name: "Piet",
           email: "p@p.com",
-          password: "piet123",
+          password: bcrypt.hashSync("123456", 10),
+          // bcrypt.hashSync(password, 10)
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "Miriam",
+          email: "m@m.com",
+          password: bcrypt.hashSync("miriam", 10),
+          // bcrypt.hashSync(password, 10)
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "Gonzalo",
+          email: "g@g.com",
+          password: bcrypt.hashSync("gonzalo", 10),
+          // bcrypt.hashSync(password, 10)
           createdAt: new Date(),
           updatedAt: new Date(),
         },
